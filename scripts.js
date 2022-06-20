1003552536430deb84909c027e10b574f99e969d
Anderson Lima <105341229+anderson-marcelo-bastos-lima@users.noreply.github.com>
2022-06-20 16:14:04 +1000

    Team Member 1 updates.

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* Create a while loop that counts down from 10 to 1, log the value to the console. */
+
+
+i=10;
+
+while (i >= 1) {
+    console.log(i);
+    i--;
+};
+
+
+const myFruitSalad = ['apples','oranges','plums','peaches'];
+console.log(myFruitSalad[3]); // the last element using indexing
